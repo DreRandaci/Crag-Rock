146f696b3322769a1fb961974e9056f17ca78a14
Dre mf Randaci <drerandaci@gmail.com>
2017-12-05 00:07:48 -0600

    single marker displaying

--- javascripts/controllers/TripCtrl.js
+++ javascripts/controllers/TripCtrl.js
@@ -1,16 +1,48 @@
 'use strict';
 
-app.controller('TripCtrl', function ( $scope, uiGmapGoogleMapApi ) {
-    
-    uiGmapGoogleMapApi.then((stuff) => {
-        console.log(stuff);
-        $scope.map = { 
-            center: { 
-                latitude: 45, 
-                longitude: -73 
-            }, 
-            zoom: 8 
-        };
-    });
+app.controller('TripCtrl', function ( $http, $q, $log, $scope, MapsService ) {    
+
+    $scope.map = {
+        center: {
+            latitude: 36.1626638, longitude: -86.7816016
+        }, 
+        zoom: 8,
+    };
+
+    var address = 'nashville, tn';    
+    const geocode = (address) => {
+        MapsService.getMapByAddressQuery(address).then((results) => {
+            console.log(results);
+        }).catch((err) => {
+            console.log("error in getMapByAddressQuery:", err);
+        });
+    };
+    geocode(address);
+
+    $scope.marker = {
+        id: 0,
+        coords: {
+                    //nashville
+          latitude: 36.1626638,
+          longitude: -86.7816016
+        },
+        options: { draggable: true },
+        events: {
+          dragend: function (marker, eventName, args) {
+            $log.log('marker dragend');
+            var lat = marker.getPosition().lat();
+            var lon = marker.getPosition().lng();
+            $log.log(lat);
+            $log.log(lon);
+  
+            $scope.marker.options = {
+              draggable: true,
+              labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+              labelAnchor: "100 0",
+              labelClass: "marker-labels"
+            };
+          }
+        }
+      };
 
 });
