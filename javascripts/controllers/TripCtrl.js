92bbf581b971b8ea1648cc4eb0bbf2e20ad98ca0
Dre mf Randaci <drerandaci@gmail.com>
2017-12-05 13:45:55 -0600

    map search query

--- javascripts/controllers/TripCtrl.js
+++ javascripts/controllers/TripCtrl.js
@@ -1,48 +1,131 @@
 'use strict';
 
-app.controller('TripCtrl', function ( $http, $q, $log, $scope, MapsService ) {    
-
+app.controller('TripCtrl', function ($http, $q, $log, $scope, MapsService) {
+    
+    //initial map instance on page load
     $scope.map = {
         center: {
             latitude: 36.1626638, longitude: -86.7816016
-        }, 
+        },
         zoom: 8,
+        options: {
+            scrollwheel: true
+        }
     };
 
-    var address = 'nashville, tn';    
-    const geocode = (address) => {
+    // initial marker instance on page load
+    $scope.marker = {
+        id: 0,
+        coords: {
+            latitude: 36.1626638, longitude: -86.7816016
+        },
+        options: { draggable: true },
+        events: {
+            dragend: function (marker, eventName, args) {
+                $log.log('marker drag-end');
+                let lat = marker.getPosition().lat();
+                let lon = marker.getPosition().lng();
+                $log.log(lat);
+                $log.log(lon);
+
+                $scope.marker.options = {
+                    draggable: true,
+                    labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                    labelAnchor: "100 0",
+                    labelClass: "marker-labels"
+                };
+            }
+        }
+    };
+
+    //for geolocation later on
+    const getCurrentLocationMap = () => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            
+            //update map instance on page load with geolcation
+            $scope.map = {
+                center: {
+                    latitude: position.coords.latitude, longitude: position.coords.longitude
+                },
+                zoom: 8,
+                options: {
+                    scrollwheel: true
+                }
+            };
+
+            //update marker instance on page load with geolocation
+            $scope.marker = {
+                id: 0,
+                coords: {
+                    latitude: position.coords.latitude, longitude: position.coords.longitude
+                },
+                options: { draggable: true },
+                events: {
+                    dragend: function (marker, eventName, args) {
+                        $log.log('marker drag-end');
+                        let lat = marker.getPosition().lat();
+                        let lon = marker.getPosition().lng();
+                        $log.log(lat);
+                        $log.log(lon);
+
+                        $scope.marker.options = {
+                            draggable: true,
+                            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                            labelAnchor: "100 0",
+                            labelClass: "marker-labels"
+                        };
+                    }
+                }
+            };
+        });
+    };
+
+    //grab search query and update map marker
+    $scope.geocode = (address) => {
         MapsService.getMapByAddressQuery(address).then((results) => {
-            console.log(results);
+            console.log(results.data.results[0]);
+
+            let lat = results.data.results[0].geometry.location.lat;
+            let lng = results.data.results[0].geometry.location.lng;
+
+            $scope.map = {
+                center: {
+                    latitude: lat,
+                    longitude: lng
+                },
+                zoom: 11,
+                options: {
+                    scrollwheel: true
+                }
+            };
+
+            $scope.marker = {
+                id: 0,
+                coords: {
+                    latitude: lat,
+                    longitude: lng
+                },
+                options: { draggable: true },
+                events: {
+                    dragend: function (marker, eventName, args) {
+                        $log.log('marker drag-end');
+                        let lat = marker.getPosition().lat();
+                        let lon = marker.getPosition().lng();
+                        $log.log(lat);
+                        $log.log(lon);
+
+                        $scope.marker.options = {
+                            draggable: true,
+                            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                            labelAnchor: "100 0",
+                            labelClass: "marker-labels"
+                        };
+                    }
+                }
+            };
         }).catch((err) => {
             console.log("error in getMapByAddressQuery:", err);
         });
     };
-    geocode(address);
-
-    $scope.marker = {
-        id: 0,
-        coords: {
-                    //nashville
-          latitude: 36.1626638,
-          longitude: -86.7816016
-        },
-        options: { draggable: true },
-        events: {
-          dragend: function (marker, eventName, args) {
-            $log.log('marker dragend');
-            var lat = marker.getPosition().lat();
-            var lon = marker.getPosition().lng();
-            $log.log(lat);
-            $log.log(lon);
-  
-            $scope.marker.options = {
-              draggable: true,
-              labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-              labelAnchor: "100 0",
-              labelClass: "marker-labels"
-            };
-          }
-        }
-      };
 
 });
--- javascripts/services/MapsService.js
+++ javascripts/services/MapsService.js
@@ -3,30 +3,7 @@
 app.service("MapsService", function( $http, $q, GOOGLEMAPS_CONFIG ){
 
     const getMapByAddressQuery = (address) => {
-        return $q(( resolve, reject ) => {
-            $http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLEMAPS_CONFIG}`).then((results) => {
-                console.log("geocode results:", results.data.results[0]);
-                
-                // let lat = results.results[0].geometry.location.lat;
-                // let lng = results.results[0].geometry.location.lng;
-
-                // MountainProjService.getClimbs(lat, lng).then().catch();
-
-                // let loc = {
-                //     lat: lat, 
-                //     lng: lng
-                // };
-                // let map = new google.maps.Map(document.getElementById('map'), {
-                //     zoom: 8,
-                //     center: loc
-                // });
-
-                // let marker = new google.maps.Marker({
-                //     position: loc,
-                //     map: map
-                // });
-            });
-        });
+        return $http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLEMAPS_CONFIG}`);
     };
     return { getMapByAddressQuery };
 });
