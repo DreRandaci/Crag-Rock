3d56ac6cf3dcef19c05a8ec2dea183143828371c
Dre mf Randaci <drerandaci@gmail.com>
2017-12-05 18:39:16 -0600

    auth form and popup working

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,46 +1,79 @@
 'use strict';
 
-app.run(function( FIREBASE_CONFIG ){
-    firebase.initializeApp(FIREBASE_CONFIG); 
+let isAuth = (AuthService) => new Promise((resolve, reject) => {
+    if (AuthService.isAuthenticated()) {
+        resolve();
+    } else {
+        reject();
+    }
 });
 
-app.config(function( $routeProvider, uiGmapGoogleMapApiProvider, GOOGLEMAPS_CONFIG){    
-    uiGmapGoogleMapApiProvider.configure({
-            china: true,
-            key: GOOGLEMAPS_CONFIG,
-            v: '3.30', 
-            libraries: 'weather,geometry,visualization'
-    });    
-    $routeProvider
-    .when( "/auth", {
-        templateUrl: 'partials/auth.html',
-        controller: 'AuthCtrl'
-    })
-    .when( "/chat", {
-        templateUrl: 'partials/chat.html',
-        controller: 'ChatCtrl', 
-    })
-    .when( "/friend/detail/:id", {
-        templateUrl: 'partials/friend-detail.html',
-        controller: 'FriendDetailCtrl', 
-    })
-    .when( "/friends", {
-        templateUrl: 'partials/friends.html',
-        controller: 'FriendsCtrl', 
-    })
-    .when( "/profile", {
-        templateUrl: 'partials/profile.html',
-        controller: 'ProfileCtrl', 
-    })
-    .when( "/trip/detail/:id", {
-        templateUrl: 'partials/trip-detail.html',
-        controller: 'TripDetailCtrl', 
-    })
-    .when( "/trips", {
-        templateUrl: 'partials/trips.html',
-        controller: 'TripCtrl', 
-    })
+app.run(function ($rootScope, $location, FIREBASE_CONFIG, AuthService) {
+    firebase.initializeApp(FIREBASE_CONFIG);
 
-    .otherwise('/auth');
+    //watch method that fires on change of a route.  3 inputs. 
+    //event is a change event
+    //currRoute is information about your current route
+    //prevRoute is information about the route you came from
+    $rootScope.$on('$routeChangeStart', function (event, currRoute, prevRoute) {
+        // checks to see if there is a current user
+        var logged = AuthService.isAuthenticated();
+
+        var appTo;
+
+        // to keep error from being thrown on page refresh
+        if (currRoute.originalPath) {
+            // check if the user is going to the auth page = currRoute.originalPath
+            // if user is on auth page then appTo is true
+            // if it finds something other than /auth it return a -1 and -1!==-1 so resolves to false
+            appTo = currRoute.originalPath.indexOf('/auth') !== -1;
+        }
+
+        //if not on /auth page AND not logged in redirect to /auth
+        if (!appTo && !logged) {
+            event.preventDefault();
+            $location.path('/auth');
+        }
+    });
+});
+
+app.config(function ($routeProvider, uiGmapGoogleMapApiProvider, GOOGLEMAPS_CONFIG) {
+    uiGmapGoogleMapApiProvider.configure({
+        china: true,
+        key: GOOGLEMAPS_CONFIG,
+        v: '3.30',
+        libraries: 'weather,geometry,visualization'
+    });
+    $routeProvider
+        .when("/auth", {
+            templateUrl: 'partials/auth.html',
+            controller: 'AuthCtrl'
+        })
+        .when("/chat", {
+            templateUrl: 'partials/chat.html',
+            controller: 'ChatCtrl',
+        })
+        .when("/friend/detail/:id", {
+            templateUrl: 'partials/friend-detail.html',
+            controller: 'FriendDetailCtrl',
+        })
+        .when("/friends", {
+            templateUrl: 'partials/friends.html',
+            controller: 'FriendsCtrl',
+        })
+        .when("/profile", {
+            templateUrl: 'partials/profile.html',
+            controller: 'ProfileCtrl',
+        })
+        .when("/trip/detail/:id", {
+            templateUrl: 'partials/trip-detail.html',
+            controller: 'TripDetailCtrl',
+        })
+        .when("/trips", {
+            templateUrl: 'partials/trips.html',
+            controller: 'TripCtrl',
+        })
+
+        .otherwise('/auth');
 
 });
--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,5 +1,15 @@
 'use strict';
 
-app.controller('AuthCtrl', function( $scope ){
-    $scope.test = 'AuthCtrl';
+app.controller("AuthCtrl", function ($location, $rootScope, $scope, AuthService) {
+    $scope.authenticate = (userInfo) => {
+        console.log("userInfo:", userInfo);
+        AuthService.authenticateGoogle().then((result) => {
+            $rootScope.uid = result.user.uid;
+            $scope.$apply(() => {
+                $location.url("/chat");
+            });
+        }).catch((error) => {
+            console.log("error in authenticateGoogle", error);
+        });
+    };
 });
