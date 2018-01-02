fb13f37bc1889b1f7a07d3b37e6827ffcb964d44
Dre mf Randaci <drerandaci@gmail.com>
2018-01-01 18:06:49 -0600

    auth modal route pref selection disable

--- javascripts/controllers/ProfileCtrl.js
+++ javascripts/controllers/ProfileCtrl.js
@@ -1,8 +1,26 @@
 'use strict';
 
-app.controller("ProfileCtrl", function( $rootScope, $scope, AuthService ) {
+app.controller("ProfileCtrl", function ($rootScope, $scope, AuthService) {
     // console.log($rootScope.user);
 
     $scope.user = AuthService.getCurrentUserInfo();
-    
+
+    $scope.preferences = [{ type: 'Sport' }, { type: 'Boulder' }, { type: 'Trad' }, { type: 'Alpine' }];
+    $scope.selectAll = [{ disabled: false }];
+    $scope.togglePreference = (index, pref) => {
+        $scope.preferences[index].disabled = $scope.preferences[index].disabled ? false : true;
+    };
+
+    $scope.toggleAllPreferences = (all) => {
+        $scope.selectAll[0].disabled = all.disabled ? false : true;
+        if (all.disabled) {
+            $scope.preferences.forEach((pref) => {
+                pref.disabled = true;
+            });
+        } else {
+            $scope.preferences.forEach((pref) => {
+                pref.disabled = false;
+            });
+        }
+    };
 });
